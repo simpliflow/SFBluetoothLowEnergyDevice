dfcf18206e0f755db7f37f4013571ce50cb72552
Thomas Billicsich <thomas.billicsich@simpliflow.com>
2014-04-07 16:08:54 +0200

    Added pod to example project

--- Example App/SFBLEDeviceExample/SFAppDelegate.h
+++ Example App/SFBLEDeviceExample/SFAppDelegate.h
@@ -2,9 +2,9 @@
 //  SFAppDelegate.h
 //  SFBLEDeviceExample
 //
-//  Created by Thomas Billicsich on 07.04.14.
-//  Copyright (c) 2014 SimpliFlow. All rights reserved.
-//
+//  Created by Thomas Billicsich on 2014-04-07.
+//  Copyright (c) 2014 Thomas Billicsich. All rights reserved.
+
 
 #import <UIKit/UIKit.h>
 
--- Example App/SFBLEDeviceExample/SFViewController.m
+++ Example App/SFBLEDeviceExample/SFViewController.m
@@ -2,9 +2,8 @@
 //  SFViewController.m
 //  SFBLEDeviceExample
 //
-//  Created by Thomas Billicsich on 07.04.14.
-//  Copyright (c) 2014 SimpliFlow. All rights reserved.
-//
+//  Created by Thomas Billicsich on 2014-04-07.
+//  Copyright (c) 2014 Thomas Billicsich. All rights reserved.
 
 #import "SFViewController.h"
 
